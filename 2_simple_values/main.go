8c2ca89dd2180b7196b9f18009e6b3cfd95d557f
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-06-10 10:47:10 +0530

    first commit

---
+++ 2_simple_values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(1 + 1)
+
+	//string
+	fmt.Println("Hello, " + "World!")
+
+	// boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+
+	// float
+	fmt.Println(3.14)
+}
